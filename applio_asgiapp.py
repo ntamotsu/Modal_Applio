080658bfb8b8d8af7231055ffc329b21e91aa6f3
ntamotsu <n.tamotsu318@gmail.com>
2024-06-06 17:44:15 +0900

    uvを導入

--- applio_asgiapp.py
+++ applio_asgiapp.py
@@ -12,6 +12,10 @@ local_datasets_dir = ""
 custom_pretrained_urls = [
     "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch3/resolve/main/D_KLMv7s_Batch3F_48k.pth",
     "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch3/resolve/main/G_KLMv7s_Batch3F_48k.pth",
+    "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch4/resolve/main/D_KLMv7s_Batch4_48k.pth",
+    "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch4/resolve/main/G_KLMv7s_Batch4_48k.pth",
+    "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch4/resolve/main/D_KLM4T_48k.pth",
+    "https://huggingface.co/SeoulStreamingStation/KLMv7s_Batch4/resolve/main/G_KLM4T_48k.pth",
     # 適宜追加・削除してください
 ]
 
@@ -21,7 +25,9 @@ app_name = "applio"
 TAG_NAME = "3.2.0"
 REMOTE_LOGS_DIR = "/root/logs"
 REMOTE_DATASETS_DIR = "/root/assets/datasets"
+REMOTE_REQUIREMENTS_FILE = "/root/requirements_applio.txt"
 LOCAL_MUTE_DIR = Path(__file__).parent / "mute"
+LOCAL_REQUIREMENTS_FILE = Path(__file__).parent / "requirements_applio.txt"
 
 
 vol = modal.Volume.from_name(logs_volume_name, create_if_missing=True)
@@ -37,14 +43,12 @@ except AssertionError as e:
 image: modal.Image = (
     modal.Image.debian_slim(python_version="3.10")
     .apt_install("git", "ffmpeg")
+    .pip_install("uv")
     .run_commands(
         "cd /root && git init .",
         "cd /root && git remote add --fetch origin https://github.com/IAHispano/Applio.git",
         f"cd /root && git checkout {TAG_NAME}",
         f"rm -rf {REMOTE_LOGS_DIR}/mute",  # Volumeは空のディレクトリにしかマウントできないため、既存のmuteディレクトリは一旦削除
-        "cd /root && pip install -r requirements.txt",
-        "cd /root && pip uninstall torch torchvision torchaudio -y",
-        "cd /root && pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1",
     )
     .env(
         {
@@ -67,17 +71,31 @@ app = modal.App(name=app_name, image=image)
     _allow_background_volume_commits=True,
     mounts=(
         [
-            modal.Mount.from_local_dir(
-                local_datasets_dir, remote_path=REMOTE_DATASETS_DIR
+            modal.Mount.from_local_file(
+                LOCAL_REQUIREMENTS_FILE,
+                remote_path=REMOTE_REQUIREMENTS_FILE,
             )
         ]
-        if local_datasets_dir
-        else ()
+        + (
+            [
+                modal.Mount.from_local_dir(
+                    local_datasets_dir, remote_path=REMOTE_DATASETS_DIR
+                )
+            ]
+            if local_datasets_dir
+            else []
+        )
     ),
 )
 class Model:
     @modal.build()
     def model_preload(self):
+        import subprocess
+
+        subprocess.run(
+            f"uv pip install --system -r {REMOTE_REQUIREMENTS_FILE}", shell=True
+        )
+
         from core import run_prerequisites_script  # type: ignore
         import wget  # type: ignore
         import os
@@ -96,107 +114,7 @@ class Model:
 
     @modal.asgi_app()
     def launch_gradio(self):
-        """Applioのapp.pyの内容ほぼそのまま"""
-        import gradio as gr  # type: ignore
-        import sys
-        import os
-        import logging
-
-        now_dir = os.getcwd()
-        sys.path.append(now_dir)
-
-        # Tabs
-        from tabs.inference.inference import inference_tab  # type: ignore
-        from tabs.train.train import train_tab  # type: ignore
-        from tabs.extra.extra import extra_tab  # type: ignore
-        from tabs.report.report import report_tab  # type: ignore
-        from tabs.download.download import download_tab  # type: ignore
-        from tabs.tts.tts import tts_tab  # type: ignore
-        from tabs.voice_blender.voice_blender import voice_blender_tab  # type: ignore
-        from tabs.settings.presence import presence_tab, load_config_presence  # type: ignore
-        from tabs.settings.flask_server import flask_server_tab  # type: ignore
-        from tabs.settings.fake_gpu import fake_gpu_tab, gpu_available, load_fake_gpu  # type: ignore
-        from tabs.settings.themes import theme_tab  # type: ignore
-        from tabs.plugins.plugins import plugins_tab  # type: ignore
-        from tabs.settings.version import version_tab  # type: ignore
-        from tabs.settings.lang import lang_tab  # type: ignore
-        from tabs.settings.restart import restart_tab  # type: ignore
-
-        # Assets
-        import assets.themes.loadThemes as loadThemes  # type: ignore
-        from assets.i18n.i18n import I18nAuto  # type: ignore
-        import assets.installation_checker as installation_checker  # type: ignore
-        from assets.discord_presence import RPCManager  # type: ignore
-        from assets.flask.server import start_flask, load_config_flask  # type: ignore
-
-        i18n = I18nAuto()
-        if load_config_presence() == True:
-            RPCManager.start_presence()
-        installation_checker.check_installation()
-        logging.getLogger("uvicorn").disabled = True
-        logging.getLogger("fairseq").disabled = True
-        if load_config_flask() == True:
-            print("Starting Flask server")
-            start_flask()
-
-        my_applio = loadThemes.load_json()
-        if my_applio:
-            pass
-        else:
-            my_applio = "ParityError/Interstellar"
-
-        with gr.Blocks(theme=my_applio, title="Applio") as Applio:
-            gr.Markdown("# Applio")
-            gr.Markdown(
-                i18n(
-                    "Ultimate voice cloning tool, meticulously optimized for unrivaled power, modularity, and user-friendly experience."
-                )
-            )
-            gr.Markdown(
-                i18n(
-                    "[Support](https://discord.gg/IAHispano) — [Discord Bot](https://discord.com/oauth2/authorize?client_id=1144714449563955302&permissions=1376674695271&scope=bot%20applications.commands) — [Find Voices](https://applio.org/models) — [GitHub](https://github.com/IAHispano/Applio)"
-                )
-            )
-            with gr.Tab(i18n("Inference")):
-                inference_tab()
-
-            with gr.Tab(i18n("Train")):
-                if gpu_available() or load_fake_gpu():
-                    train_tab()
-                else:
-                    gr.Markdown(
-                        i18n(
-                            "Training is currently unsupported due to the absence of a GPU. To activate the training tab, navigate to the settings tab and enable the 'Fake GPU' option."
-                        )
-                    )
-
-            with gr.Tab(i18n("TTS")):
-                tts_tab()
-
-            with gr.Tab(i18n("Voice Blender")):
-                voice_blender_tab()
-
-            with gr.Tab(i18n("Plugins")):
-                plugins_tab()
-
-            with gr.Tab(i18n("Download")):
-                download_tab()
-
-            with gr.Tab(i18n("Report a Bug")):
-                report_tab()
-
-            with gr.Tab(i18n("Extra")):
-                extra_tab()
-
-            with gr.Tab(i18n("Settings")):
-                presence_tab()
-                flask_server_tab()
-                if not gpu_available():
-                    fake_gpu_tab()
-                theme_tab()
-                version_tab()
-                lang_tab()
-                restart_tab()
+        from app import Applio  # type: ignore
 
         fast_api_app, _, _ = Applio.launch(
             favicon_path="assets/ICON.ico",
